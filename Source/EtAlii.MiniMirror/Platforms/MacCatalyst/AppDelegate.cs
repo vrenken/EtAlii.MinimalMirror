10d6c2662169b8870c70cd2ac284271d04a182ad
Peter Vrenken <github@petervrenken.nl>
2023-12-10 15:57:52 +0100

    First rough solution outline.

---
+++ Source/EtAlii.MiniMirror/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace EtAlii.MiniMirror;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
